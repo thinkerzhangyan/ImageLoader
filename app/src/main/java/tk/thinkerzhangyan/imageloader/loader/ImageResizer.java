f13fd949b40b26e09e94c4a16858a3df397e33e0
张岩 <zhangyan22@le.com>
2018-04-10 17:11:54 +0800

    添加了新的压缩加载方法。

--- app/src/main/java/tk/thinkerzhangyan/imageloader/loader/ImageResizer.java
+++ app/src/main/java/tk/thinkerzhangyan/imageloader/loader/ImageResizer.java
@@ -55,6 +55,26 @@ public class ImageResizer {
         return BitmapFactory.decodeFileDescriptor(fd, null, options);
     }
 
+    public Bitmap decodeBitmapFromFile(String path, int reqWidth, int reqHeight) {
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(path, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqHeight, reqWidth);
+
+        options.inJustDecodeBounds = false;
+
+        options.inPurgeable = true;
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        options.inInputShareable = true;
+
+        return  BitmapFactory.decodeFile(path, options);
+
+    }
+
     public int calculateInSampleSize(BitmapFactory.Options options,
             int reqWidth, int reqHeight) {
         if (reqWidth == 0 || reqHeight == 0) {
